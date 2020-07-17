b256d5f990ac2c0aae5de84000e5e6ecff258180
tellang <tellang@daum.net>
2020-07-17 16:59:52 +0900

    Gold_Pot_Pro

--- src/Gold_Pot/Algo.java
+++ src/Gold_Pot/Algo.java
@@ -5,38 +5,45 @@ public class Algo {
         this.goldPot = goldPot;
         this.A = new int[goldPot.length];
         this.B = new int[goldPot.length];
-        this.turnStack = new int[goldPot.length];
+        this.indexStack = new int[goldPot.length];
     }
-    int[] goldPot, A, B, turnStack;
+    int[] goldPot, A, B, indexStack;
     int stack = 0;
     //char turn = 'A';
 
     public void cal (int st, int fin){
         switch (fin - st){
             case 1:{
-
-                if(goldPot[fin] > goldPot[st]){
-                    turnStack[stack++] = goldPot[fin];
-                    turnStack[stack++] = goldPot[st];
-                }
-                else{
-                    turnStack[stack++] = goldPot[st];
-                    turnStack[stack++] = goldPot[fin];
-                }
+                stkMaxVIndex(st, fin);
             }
                 break;
             case 2:{
-
+                indexStack[stack++]=maxVIndex(st, fin);
+                if(indexStack[stack]==st)
+                    cal(++st, fin);
+                else
+                    cal(st, --fin);
             }
                 break;
         }
     }
 
-    public int retMax(int i, int j){
-        if(i>j)
+    public int maxVIndex(int i, int j){
+        if(goldPot[i]>goldPot[j])
             return i;
         else
             return j;
+    }
+
+    public void stkMaxVIndex(int i, int j){
+        if(goldPot[i]>goldPot[j]){
+            indexStack[stack++] = i;
+            indexStack[stack++] = j;
+        }
+        else{
+            indexStack[stack++] = j;
+            indexStack[stack++] = i;
+        }
     }
 
 
