fcd030054038f7e086014c4a9ccb122abf6edcd6
tellang <tellang@daum.net>
2020-07-20 17:39:44 +0900

    Pattern_Algo_Complete

--- src/StrangeNum/PrimeNum.java
+++ src/StrangeNum/PrimeNum.java
@@ -2,18 +2,20 @@ package StrangeNum;
 
 public class PrimeNum {
     static boolean[] prime;
-    public PrimeNum(int limit){
+
+    public PrimeNum(int limit) {
         prime = new boolean[++limit];
         prime[0] = prime[1] = true;
 
-        for(int i=2; i*i < limit; i++){
-            if(!prime[i]){
-                for(int j=i*i; j<limit; j+=i)
+        for (int i = 2; i * i < limit; i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j < limit; j += i)
                     prime[j] = true;
             }
         }
     }
-    public static boolean isPrime(int index){
+
+    public static boolean isPrime(int index) {
         return !prime[index];
     }
 }
