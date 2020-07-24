ef5826301dccc099413fee40dd85870d02c4703e
tellang <tellang@daum.net>
2020-07-24 16:14:32 +0900

    null

---
+++ src/HashTest/Solution.java
@@ -0,0 +1,20 @@
+package HashTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static String solution(String[] participant, String[] completion) {
+        List<String> loser = new ArrayList<>(Arrays.asList(participant));
+        for(String fisisher: completion)
+            loser.remove(fisisher);
+        String answer = loser.get(0);
+        return answer;
+    }
+    public static void main (String[] args){
+        String[] participant = {"a", "b", "c", "d"};
+        String[] completion = {"a", "b", "c"};
+        System.out.println(solution(participant, completion));
+    }
+}
