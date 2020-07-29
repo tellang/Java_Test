ebfcd6be507ce3e904a8e5f5672706ff17bc7a5d
tellang <tellang@daum.net>
2020-07-29 15:34:58 +0900

    Test_Clear_Version_Commit

--- src/Kakao_Internship/KeyPad.java
+++ src/Kakao_Internship/KeyPad.java
@@ -7,8 +7,9 @@ public class KeyPad {
     public static void main(String[]args){
         Solution s = new Solution();
         String hand = "right";
-        int[] num = {1, 3, 4, 5};
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         System.out.println(s.solution(num, hand));
+
     }
 }
 class Solution {
@@ -16,40 +17,62 @@ class Solution {
         Map<String, Integer> finPos = new HashMap();
         String answer = "";
         finPos.put("R", 33);
-        finPos.put("L", 13);
+        finPos.put("L", 31);
        for(int num: numbers){
-           System.out.println("rPos: "+finPos.get("R")+" lPos: "+finPos.get("L"));
            switch (num%3){
                case 0:{
-                   answer += "R";                   finPos.put("R", finPos.getOrDefault("R",calPos(num)));
+                   if(num != 0) {
+                       answer += "R";
+                       finPos.put("R", calPos(num));
+                   }
+                   else{
+                       if(disCal(finPos.get("R"), calPos(num)) > disCal(finPos.get("L"), calPos(num))){
+                           answer += "L";
+                           finPos.put("L",calPos(num));
+                       }
+                       else if(disCal(finPos.get("R"), calPos(num)) < disCal(finPos.get("L"), calPos(num))){
+                           answer += "R";
+                           finPos.put("R",calPos(num));
+                       }
+                       else {
+                           if(hand.equals("left")){
+                               answer += "L";
+                               finPos.put("L",calPos(num));
+                           }
+                           else {
+                               answer += "R";
+                               finPos.put("R",calPos(num));
+                           }
+                       }
+                   }
+
                }break;
                case 1:{
                    answer += "L";
-                   finPos.put("L", finPos.getOrDefault("L",calPos(num)));
+                   finPos.put("L",calPos(num));
                }break;
                default:{
                     if(disCal(finPos.get("R"), calPos(num)) > disCal(finPos.get("L"), calPos(num))){
                         answer += "L";
-                        finPos.put("L", finPos.getOrDefault("L",calPos(num)));
+                        finPos.put("L",calPos(num));
                     }
                     else if(disCal(finPos.get("R"), calPos(num)) < disCal(finPos.get("L"), calPos(num))){
                         answer += "R";
-                        finPos.put("R", finPos.getOrDefault("R",calPos(num)));
+                        finPos.put("R",calPos(num));
                     }
                     else {
-                        if(hand == "left"){
+                        if(hand.equals("left")){
                             answer += "L";
-                            finPos.put("L", finPos.getOrDefault("L",calPos(num)));
+                            finPos.put("L",calPos(num));
                         }
                         else {
                             answer += "R";
-                            finPos.put("R", finPos.getOrDefault("R",calPos(num)));
+                            finPos.put("R",calPos(num));
                         }
                     }
                }
            }
        }
-        //System.out.println("rPos: "+finPos.get("R")+" lPos: "+finPos.get("L"));
         return answer;
     }
 
@@ -58,14 +81,17 @@ class Solution {
         int Ax = posA - Ay*10;
         int By = posB/10;
         int Bx = posB - By*10;
-        return (Ay-By)*(Ay-By) + (Ax-Bx)*(Ax-Bx);
+        return (Ay-By)*(Ay-By) + (Ax-Bx)*(Ax-Bx); //yx
     }
 
     public static int calPos(int num){
+        int buf=0;
+        if(num%3==0)
+            buf = 1;
         if(num==0)
-            return 33;
+            return 32;
         else{
-            return num%4+num/3*10;
+            return (num-(num/3-buf)*3)%4+(num/3-buf)*10;
         }
     }
 
