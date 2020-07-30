55b5fa33e8fe1037226e42dab449f93493176cd2
tellang <tellang@daum.net>
2020-07-30 17:07:18 +0900

    Clear_Commit

--- src/Kakao_Internship/Calculater.java
+++ src/Kakao_Internship/Calculater.java
@@ -4,58 +4,76 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+import static java.lang.StrictMath.abs;
+
 public class Calculater {
     public static void main(String[] args){
-        String exp = "100-200*300-500+20";
-        char opr = '+';
+        String exp = "10*100*10000000*100-1000";
         Calculater c = new Calculater();
-        //c.solution(exp);
-        System.out.println((int)opr);
+        System.out.println("urTest: "+c.solution(exp));
+        System.out.println(stack(1, 2, 3, exp));
     }
-    public static long calculate(int mulL, int minL, int plsL, String exp){
+    public static long stack(int mulL, int plsL, int minL, String exp){
         Map<String, Integer> oprL = new HashMap<>();
         Stack<String> opr = new Stack<>();
-        Stack<Integer> num = new Stack<>();
-        int exOprIndex=0;
+        Stack<Long> num = new Stack<>();
+        int exOprIndex=-1;
 
         oprL.put("*", mulL);
         oprL.put("-", minL);
         oprL.put("+", plsL);
 
         for(int i = 0; i < exp.length();  i++){
-            //System.out.println(exp.substring(i, i+1));
             if(exp.substring(i, i+1).equals("*") || exp.substring(i, i+1).equals("+")
             || exp.substring(i, i+1).equals("-")){
-                num.push(Integer.parseInt(exp.substring(exOprIndex, i)));
-                exOprIndex = i;
-                if(oprL.get(opr.peek()) <= oprL.get(exp.substring(i, i+1)) || opr.empty())
+                num.push(Long.parseLong(exp.substring(exOprIndex+1, i)));
+                if(opr.empty())
+                    opr.push(exp.substring(i, i+1));
+                else if(oprL.get(opr.peek()) < oprL.get(exp.substring(i, i+1)))
                     opr.push(exp.substring(i, i+1));
                 else{
-                    String oprater = opr.pop();
-                    int left, right;
-                    left = num.pop();
-                    right = num.pop();
-                    if(oprater.equals("+"))
-                        num.push(left+right);
-                    else if (oprater.equals("*"))
-                        num.push(left*right);
-                    else
-                        num.push(left-right);
+                    while (!opr.empty()){
+                        if(oprL.get(opr.peek()) < oprL.get(exp.substring(i, i+1)))
+                            break;
+                        calculate(opr, num);
+                    }
+                    opr.push(exp.substring(i, i+1));
                 }
+                exOprIndex = i;
             }
 
         }
+        num.push(Long.parseLong(exp.substring(exOprIndex+1)));
 
+        while (!opr.empty()){
+            calculate(opr, num);
+        }
+        return abs(num.pop());
+    }
+
+    public static void calculate(Stack<String> opr, Stack<Long> num){
+        String oprater = opr.pop();
+        long left, right;
+        right = num.pop();
+        left = num.pop();
+        if(oprater.equals("+"))
+            num.push(left+right);
+        else if (oprater.equals("*"))
+            num.push(left*right);
+        else
+            num.push(left-right);
     }
 
     public long solution(String exp) {
-
-        /*while (true){
-            String result = expression.substring(expression.lastIndexOf("/")+1);
-        }*/
-
-
         long answer = 0;
+        int[][] lot = new int[][]{
+                {1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}
+        };
+        for(int[] lotate: lot){
+            if(stack(lotate[0], lotate[1], lotate[2], exp) > answer)
+                answer = stack(lotate[0], lotate[1], lotate[2], exp);
+        }
+
         return answer;
     }
 }
